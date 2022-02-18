9c017a40675f80f603affd9dec0e62693ea5ba73
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2022-02-18 20:20:52 +0100

    Delete language/italiano directory

--- language/italiano/italiano.py
+++
@@ -1,7 +0,0 @@
-import os
-
-#inizio codice
-print(" ciao io sono cento e sono felice di aiutarti")
-print("\n chiedimi qualcosa se no scrivi (aiuto)")
-
-os.system("python3 language/italiano/console.py")
