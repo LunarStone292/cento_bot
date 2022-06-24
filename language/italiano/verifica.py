c86424e351e5eede9814ae83c8d92d7091255b7a
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2022-06-24 13:09:14 +0200

    Update verifica.py

--- language/italiano/verifica.py
+++ language/italiano/verifica.py
@@ -1,3 +1,5 @@
+from gtts import gTTS
+from playsound import playsound
 import os
 import platform
 import time
@@ -7,22 +9,29 @@ if(platform.system() == "Windows"):
 if(platform.system() == "Linux"):
     os.system("clear")
 
-print(" linugua inpostata ------> ITALIANO")
+print(" linugua impostata ------> ITALIANO")
 print("\n do you want to change language?")
+
+text = "lingua impostata, italiano"
+
+tts = gTTS(text=text, lang='it')
+tts.save("voce.mp3")
+
+playsound("voce.mp3")
+
+os.system("del voce.mp3")
+
 confirm = input("\n (S/n): ")
 
+
 if(confirm == "S"):
+    os.system("cls")
+    os.system("del txt/log.txt")
+    os.system("type nul > txt/log.txt")
     os.system("python3 cento.py")
 if(confirm == "n"):
-    if(platform.system() == "Windows"):
-        os.system("cls")
-    if(platform.system() == "Linux"):
-        os.system("clear")
+    os.system("cls")
     os.system("python3 language/italiano/italiano.py")
-
 else:
-    if(platform.system() == "Windows"):
-        os.system("cls")
-    if(platform.system() == "Linux"):
-        os.system("clear")
+    os.system("cls")
     print("\n error")
