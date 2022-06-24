5716399f5f548fbf0129008ea019bc9c483fc9a3
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2022-06-24 13:09:35 +0200

    Update italiano.py

--- language/italiano/italiano.py
+++ language/italiano/italiano.py
@@ -1,7 +1,19 @@
 import os
+from gtts import gTTS
+from playsound import playsound
+import os
 
 #inizio codice
 print(" ciao io sono cento e sono felice di aiutarti")
 print("\n chiedimi qualcosa se no scrivi (aiuto)")
+
+text = "ciao, io sono cento e sono felice di aiutarti, chiedimi qualcosa se no scrivi aiuto"
+
+tts = gTTS(text=text, lang='it')
+tts.save("voce.mp3")
+
+playsound("voce.mp3")
+
+os.system("del voce.mp3")
 
 os.system("python3 language/italiano/console.py")
