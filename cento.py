68157188fac207cc1885cbf49fffc73894286226
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2022-06-24 13:08:46 +0200

    Update cento.py

--- cento.py
+++ cento.py
@@ -2,23 +2,33 @@ import os
 import platform
 import getpass
 
-if(platform.system() == "Windows"):
-    os.system("cls")
-    print("              _")
-    print("__      _____| | ___ ___  _ __ ___   ___ ")
-    print("\ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ ")
-    print(" \ V  V /  __/ | (_| (_) | | | | | |  __/ ")
-    print("  \_/\_/ \___|_|\___\___/|_| |_| |_|\___| ")
-
-    print("\n\n Hi " + getpass.getuser() + ", i'm cento and i'm happy to help you")
-    print("\n ---------------------------------------------")
-    print("\n italiano")
-    print("\n ---------------------------------------------")
-    language = input("\n please, enter a language : ")
-
-    if(language == "italiano"):
-        os.system("python3 language/italiano/verifica.py")
-
 if(platform.system() == "Linux"):
-    print("\n questo bot non è supportato per linux \n\n")
-    exit
+            print("\n this bot is not supported for linux \n\n")
+            exit
+
+f = open("txt/log.txt","r") 
+while True:
+    line = f.readline() 
+    if(line == ""):
+        os.system("cls")
+        print("              _")
+        print("__      _____| | ___ ___  _ __ ___   ___ ")
+        print("\ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ ")
+        print(" \ V  V /  __/ | (_| (_) | | | | | |  __/ ")
+        print("  \_/\_/ \___|_|\___\___/|_| |_| |_|\___| ")
+
+        print("\n\n Hi " + getpass.getuser() + ", i'm cento and i'm happy to help you")
+        print("\n ---------------------------------------------")
+        print("\n italiano")
+        print("\n ---------------------------------------------")
+        language = input("\n please, enter a language : ")
+
+        if(language == "italiano"):
+            os.system("echo 1 > txt/log.txt")
+            os.system("python3 language/italiano/verifica.py")
+        else:
+            print("errore")
+            os.system("python3 cento.py")
+    else:
+        os.system("python3 language/italiano/console.py")
+f.close()
