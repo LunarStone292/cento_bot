c80d6c3445401b46afb2c88187230b1fadc3f870
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2022-06-26 12:53:12 -0400

    Update modem.py

--- command/italiano/modem.py
+++ command/italiano/modem.py
@@ -1,12 +1,18 @@
 import os
+from platform import platform
 
-os.system("netsh wlan show profiles")
-
-risposta = input("nome@connessione-# ")
-
-if(risposta == ""):
-    os.system("python3 command/italiano/modem.py")
+if(platform.system() == "Windows"):
+    os.system("netsh wlan show profiles")
+    risposta = input("nome@connessione-# ")
+    if(risposta == ""):
+        os.system("python3 command/italiano/modem.py")
+    else:
+        os.system("netsh wlan show profiles " + risposta + " key=clear")
 else:
-    os.system("netsh wlan show profiles " + risposta + " key=clear")
-
+    os.system("ls /etc/NetworkManager/system-connections/")
+    risposta = input("nome@connessione-# ")
+    if(risposta == ""):
+        os.system("python3 command/italiano/modem.py")
+    else:
+        os.system("cat /etc/NetworkManager/system-connections/" + risposta)
 os.system("python3 language/italiano/console.py")
