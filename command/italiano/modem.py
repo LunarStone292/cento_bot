01092ea357f3ef2fb470c71af67b7c2c075a8647
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2022-06-26 12:53:28 -0400

    Update modem.py

--- command/italiano/modem.py
+++ command/italiano/modem.py
@@ -1,18 +1,13 @@
 import os
-from platform import platform
+import platform
+import webbrowser
 
+ricerca = input("\n inserisci cosa vuoi cercare: ")
+
+ricerca_google = "https://www.google.com/search?q=" + ricerca + "&rlz=1C1PRFI_enIT892IT892&ei=OzbxYa71DfKI9u8PsKCF4As&ved=0ahUKEwju_oTOrc_1AhVyhP0HHTBQAbwQ4dUDCA4&uact=5&oq=lol+a&gs_lcp=Cgdnd3Mtd2l6EAMyBAguEEMyBwgAELEDEEMyBwgAELEDEEMyCwguEIAEELEDEIMBMgsILhCABBCxAxCDATIFCAAQgAQyBAgAEEMyCAgAEIAEELEDMgsIABCABBCxAxCDATILCAAQgAQQsQMQgwE6BwgAEEcQsAM6BwgAELADEEM6CQgAELADEAoQQzoKCAAQ5AIQsAMYADoMCC4QyAMQsAMQQxgBSgQIQRgASgQIRhgBUMYBWJ0EYI8FaAFwAngAgAFgiAFgkgEBMZgBAKABAcgBE8ABAdoBBggAEAEYCdoBBggBEAEYCA&sclient=gws-wiz"
 if(platform.system() == "Windows"):
-    os.system("netsh wlan show profiles")
-    risposta = input("nome@connessione-# ")
-    if(risposta == ""):
-        os.system("python3 command/italiano/modem.py")
-    else:
-        os.system("netsh wlan show profiles " + risposta + " key=clear")
+    webbrowser.open(ricerca_google)
 else:
-    os.system("ls /etc/NetworkManager/system-connections/")
-    risposta = input("nome@connessione-# ")
-    if(risposta == ""):
-        os.system("python3 command/italiano/modem.py")
-    else:
-        os.system("cat /etc/NetworkManager/system-connections/" + risposta)
+    os.system(ricerca_google)
+
 os.system("python3 language/italiano/console.py")
